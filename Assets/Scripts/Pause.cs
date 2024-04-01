40850baec6c91a80b2ccae460286b6254b4d0e60
mbolger2 <mbolger@ltu.edu>
2024-04-01 00:37:46 -0400

    Game Mostly Done
    Room 3 done
    Pause menu done
    Need:
    Main Menu Scene
    Win Scene

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [Header("Pause Menu")]
+    [Tooltip("The pause menu gameObject")]
+    [SerializeField] GameObject pauseMenu;
+    [Tooltip("Bool used by other scripts")]
+    static public bool isPaused;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+
+    public void UnPauseGame()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenuScene");
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/RoomThree.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomThree : MonoBehaviour
+{
+    [Header("Portals")]
+    [Tooltip("The gameObject of the blue portal")]
+    [SerializeField] GameObject portalBlue;
+    [Tooltip("The gameObject of the orange portal")]
+    [SerializeField] GameObject portalOrange;
+
+    [Header("Spawn Points")]
+    [Tooltip("A list containing the spawnpoints of the blue portal")]
+    [SerializeField] List<Transform> blueSpawnPoints;
+    [Tooltip("A list containing the spawnpoints of the orange portal")]
+    [SerializeField] List<Transform> orangeSpawnPoints;
+
+    bool roomActive = false;
+    bool portalEnabled = false;
+    //float portalHoldTime = 3f;
+    //float portalDisableTime = .15f;
+    float time;
+    bool checkPointOne = true;
+    bool checkPointTwo = true;
+    bool checkPointThree = true;
+
+    void Update()
+    {
+        if (roomActive)
+        {
+            time += Time.deltaTime;
+            MovingPortal();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            roomActive = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            roomActive = false;
+            SceneManager.LoadScene("WinScene");
+        }
+    }
+
+    void EnableDisablePortal()
+    {
+        portalBlue.SetActive(portalEnabled);
+        portalOrange.SetActive(portalEnabled);
+        portalEnabled = !portalEnabled;
+    }
+
+    void MovingPortal()
+    {
+        if (time >= 0)
+        {
+            if (checkPointOne)
+            {
+                // Disable portals
+                EnableDisablePortal();
+
+                // Set position and rotation
+                PortalPos(0);
+
+                // Enable portals
+                EnableDisablePortal();
+
+                // Disable the movement portion
+                checkPointOne = false;
+            }
+
+            else if (time >= 6f && !checkPointOne)
+            {
+                if (checkPointTwo)
+                {
+                    // Disable portals
+                    EnableDisablePortal();
+
+                    // Set position and rotation
+                    PortalPos(2);
+
+                    // Enable portals
+                    EnableDisablePortal();
+
+                    // Disable the movement portion
+                    checkPointTwo = false;
+                }
+
+                else if (time >= 12f && !checkPointTwo)
+                {
+                    if (checkPointThree)
+                    {
+                        // Disable portals
+                        EnableDisablePortal();
+
+                        // Set position and rotation
+                        PortalPos(1);
+
+                        // Enable portals
+                        EnableDisablePortal();
+
+                        // Disable the movement portion
+                        checkPointThree = false;
+                    }
+
+                    else if (time >= 18f && !checkPointThree)
+                    {
+                        checkPointOne = true;
+                        checkPointTwo = true;
+                        checkPointThree = true;
+                        time = 0;
+                    }
+                }
+                
+            }
+        }
+    }
+
+    void PortalPos(int i)
+    {
+        // Set position and rotation
+        portalOrange.transform.position = orangeSpawnPoints[0].transform.position;
+        portalOrange.transform.rotation = orangeSpawnPoints[0].transform.rotation;
+
+        portalBlue.transform.position = blueSpawnPoints[i].transform.position;
+        portalBlue.transform.rotation = blueSpawnPoints[i].transform.rotation;
+    }
+}
